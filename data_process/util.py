6e2b9f7ee43060cec3cb5e262d4a26c7aa617949
wuhaozhe <wuhz19@mails.tsinghua.edu.cn>
2021-04-20 17:48:42 +0800

    add multiprocess for fine slice of videos

---
+++ data_process/util.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.append("..")
+import face_alignment
+import librosa
+from deepspeech import get_prob
+
+def video_lm68_detect(video_path):
+    pass
+
+def video_lm5_detect(video_path):
+    pass
