69dc6e7365f3f1592a0de784af619b3c8121012c
wuhaozhe <wuhz19@mails.tsinghua.edu.cn>
2021-04-29 15:03:31 +0800

    add landmark detection for ted dataset

--- data_process/process_ted.py
+++ data_process/process_ted.py
@@ -3,7 +3,10 @@ import os
 import lmdb
 import multiprocessing
 import time
+import imageio
+import pickle as pkl
 from moviepy.editor import VideoFileClip
+from tqdm import tqdm
 
 '''
     the process of ted hd dataset contains the following stages:
@@ -49,23 +52,32 @@ def coarse_slice():
                             bitrate = "1500k", 
                             fps = 25)
 
-def fine_slice_worker(wid, folder, src_video_path):
+def detect_worker(wid, folder_list, src_video_path):
 
-    print(wid, folder)
     os.environ["CUDA_VISIBLE_DEVICES"] = str(wid % 4)
-    print(os.environ["CUDA_VISIBLE_DEVICES"])
-    # lmdb_path = "../data/ted_hd/lmdb"
-    # if not os.path.exists(lmdb_path):
-    #     os.makedirs(lmdb_path)
+    src_video_path = "../data/ted_hd/slice_video_coarse"
+    fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._3D, network_size=4, device='cuda')
 
-    # env = lmdb.open(lmdb_path, map_size=1099511627776)
+    if wid == 0:
+        folder_list = tqdm(folder_list)
+
+    for folder_name in folder_list:
+        folder_path = os.path.join(src_video_path, folder_name)
+        file_list = os.listdir(folder_path)
+        for file_name in file_list:
+            if file_name.endswith(".mp4"):
+                file_path = os.path.join(folder_path, file_name)
+                vid = imageio.get_reader(file_path,  'ffmpeg')
+                lm_list = []
+                for image in vid.iter_data():
+                    preds = fa.get_landmarks(image)
+                    lm_list.append(preds)
+                pkl.dump(lm_list, open(file_path[:-4] + "_lm.pkl", 'wb'))
 
     
-def fine_slice(wnum = 8):
+def detect(wnum = 8):
     '''
         detect face landmarks
-        cut slices to more fine-grained piece according to scene change and face size
-        save results to folders
         param:
         wnum: the number of threads for processing
     '''
@@ -80,9 +92,9 @@ def fine_slice(wnum = 8):
             end_idx = len(folder_list)
         else:
             end_idx = (len(folder_list) // wnum) * (wid + 1)
-        sub_folder = folder_list[start_idx: end_idx]
+        sub_folder_list = folder_list[start_idx: end_idx]
 
-        p = multiprocessing.Process(target = fine_slice_worker, args = (wid, sub_folder, src_video_path))
+        p = multiprocessing.Process(target = detect_worker, args = (wid, sub_folder_list, src_video_path))
         p_array.append(p)
         p.start()
         # force sleep 3 seconds of eahc thread to prevent conflicts when face_alignment model occupies GPU
@@ -91,10 +103,39 @@ def fine_slice(wnum = 8):
     for i in range(wnum):
         p_array[i].join()
 
+def fine_slice():
+    '''
+        cut slices to more fine-grained piece according to scene change and face size
+        save results to folders
+    '''
+    pass
+
+def get_features_worker(wid):
+    lmdb_path = "../data/ted_hd/lmdb"
+    if not os.path.exists(lmdb_path):
+        os.makedirs(lmdb_path)
+
+    env = lmdb.open(lmdb_path, map_size=1099511627776)
+
+def get_features(wnum = 8):
+    '''
+        For video:
+            reconstruct 3d param
+            normalize pose
+            filter pose
+            mean and std for calculating style code
+            rgb texture
+            uv map
+        For audio:
+            deepspeech feature
+            energy feature
+    '''
+    pass
 
 def main():
     # coarse_slice()
-    fine_slice()
+    detect()
+    # fine_slice()
 
 if __name__ == "__main__":
     main()
--- data_process/util.py
+++ data_process/util.py
@@ -7,5 +7,5 @@ from deepspeech import get_prob
 def video_lm68_detect(video_path):
     pass
 
-def video_lm5_detect(video_path):
+def lm68_2_lm5(video_path):
     pass
